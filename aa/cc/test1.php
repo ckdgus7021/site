7e95ec1cf43387f9118a60371efd341aacdc38ae
ckdgus7021 <ckdgus7021@naver.com>
2022-05-12 16:15:56 +0900

    a

--- aa/cc/test1.php
+++ aa/cc/test1.php
@@ -186,7 +186,7 @@ if (mysqli_num_rows($result) > 0) {
               if(strlen($title)>30)
               { 
                 //title이 30을 넘어서면 ...표시
-                $title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
+                $title=str_replace($board["title"],mb_substr($board["title"],0,15,"utf-8")."...",$board["title"]);
               }
         ?>
         <tbody>
